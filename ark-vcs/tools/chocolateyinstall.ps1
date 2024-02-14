646a25768f825a2d157547e2e476e20b29cf1df9
Sam Rowlands <sampricegd@protonmail.com>
2024-02-14 22:25:37 +0000

    Update chocolateyinstall.ps1

--- ark-vcs/tools/chocolateyinstall.ps1
+++ ark-vcs/tools/chocolateyinstall.ps1
@@ -3,8 +3,6 @@
 #   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
 $ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 $url        = 'https://ark-vcs.com/versions/Ark-Vcs-0_6_1.zip'
 $version = '0.6.1'
 $packageArgs = @{
@@ -12,7 +10,6 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  #file         = $fileLocation
   softwareName  = 'ark-vcs*'
   checksum      = '4F7CA783907D6FE19178C05F0CFE8AFC1FAFA78019CD7B25DF27FEFD01D477D9'
   checksumType  = 'sha256'

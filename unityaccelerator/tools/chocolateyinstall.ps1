e467963be3513b35b13e6390f5e2e08a1e7223e3
Sam Rowlands <srowlands.dev@proton.me>
2024-02-16 23:25:21 +0000

    [UPDATE] Updating the install script for the unity accelerator choco package

--- unityaccelerator/tools/chocolateyinstall.ps1
+++ unityaccelerator/tools/chocolateyinstall.ps1
@@ -1,45 +1,26 @@
 ﻿$ErrorActionPreference = 'Stop'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-#$fileLocation = Join-Path $toolsDir 'NAME_OF_EMBEDDED_INSTALLER_FILE'
-# If embedding binaries increase total nupkg size to over 1GB, use share location or download from urls
-#$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
-$url        = '' # download url, HTTPS preferred
-$packageArgs = @{
+$url        = 'https://accelerator.cloud.unity3d.com/api/v1/accelerator/download/installer?target_os=windows&download_location=manual'
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  #file         = $fileLocation
   softwareName  = 'unityaccelerator*'
-  # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
-  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  silentArgs   = '/S'           # NSIS
-  #silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
-  #silentArgs   = '/s'           # InstallShield
-  #silentArgs   = '/s /v"/qn"'   # InstallShield with MSI
-  #silentArgs   = '/s'           # Wise InstallMaster
-  #silentArgs   = '-s'           # Squirrel
-  #silentArgs   = '-q'           # Install4j
-  #silentArgs   = '-s'           # Ghost
+  checksum      = 'D6ABBFBD4B7F1E84E2E9833A912B86FFC5B9CA6165ED4FE874D0071181B55353'
+  checksumType  = 'sha256'
+  silentArgs   = '/s /v"/qn"'
   validExitCodes= @(0)
 }
 
 Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
-#Install-ChocolateyZipPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyzippackage
 ## If you are making your own internal packages (organizations), you can embed the installer or
 ## put on internal file share and use the following instead (you'll need to add $file to the above)
 #Install-ChocolateyInstallPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
 
 ## Install an application, will assert administrative rights
-## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
-## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
-## add additional optional arguments as necessary
 ##Install-ChocolateyPackage $packageName $fileType $silentArgs $url [$url64 -validExitCodes $validExitCodes -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64]
 
 ## Installer, will assert administrative rights - used by Install-ChocolateyPackage
-## use this for embedding installers in the package when not going to community feed or when you have distribution rights
-## - https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
 #Install-ChocolateyInstallPackage $packageName $fileType $silentArgs '_FULLFILEPATH_' -validExitCodes $validExitCodes
 
 ## Runs processes asserting UAC, will assert administrative rights - used by Install-ChocolateyInstallPackage

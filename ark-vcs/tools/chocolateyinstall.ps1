8f97e966e84708b67dc6d464fef067cecbe7b17a
Sam Rowlands <srowlands.dev@proton.me>
2024-03-13 22:54:09 +0000

    [UPDATE] Updating version numbers and sha256 checksum

--- ark-vcs/tools/chocolateyinstall.ps1
+++ ark-vcs/tools/chocolateyinstall.ps1
@@ -1,15 +1,15 @@
 $ErrorActionPreference = 'Stop'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://ark-vcs.com/versions/Ark-Vcs-0_6_2.zip'
-$version = '0.6.2'
+$url        = 'https://ark-vcs.com/versions/Ark-Vcs-0_6_1.zip'
+$version = '0.6.1'
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'ark-vcs*'
-  checksum      = '301CBD37F59BC06FD06B098A457E86E39737F68A48F07413517DBED60015B3AC'
+  checksum      = '4F7CA783907D6FE19178C05F0CFE8AFC1FAFA78019CD7B25DF27FEFD01D477D9'
   checksumType  = 'sha256'
   validExitCodes= @(0)
   silentArgs   = '/s /v"/qn"'
